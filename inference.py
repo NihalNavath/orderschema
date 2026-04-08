1514e9a9af86a9b6b14d04a32e5de0943390a394
Nihal <66069728+NihalNavath@users.noreply.github.com>
2026-04-08 09:10:14 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -10,6 +10,7 @@ from orderschema import OrderschemaEnv, OrderschemaAction
 
 # ---------------- CONFIG ----------------
 LOCAL_IMAGE_NAME = os.getenv("LOCAL_IMAGE_NAME") or "orderschema"
+IMAGE_NAME = os.getenv("IMAGE_NAME") or "orderschema"
 API_KEY = os.getenv("HF_TOKEN") or os.getenv("API_KEY")
 API_BASE_URL = os.getenv("API_BASE_URL") or "https://router.huggingface.co/v1"
 MODEL_NAME = os.getenv("MODEL_NAME") or "Qwen/Qwen2.5-72B-Instruct"
